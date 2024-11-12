c413a73230189a5975d4afcb8f06f9efa3f9cd83
radioliver <radi.oliver@diak.szbi-pg.hu>
2024-11-12 10:46:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+orders = []
 
 
 @app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
-@app.route('/pizza-order', methods=["GET"])
+@app.route('/pizza-order', methods=["GET", "POST"])
 def pizza_order():
-    return render_template('pizza_order.html')
-
-
-
-
-
-
-
+    if request.method == "POST":
+        _name = request.form.get("name")
+        _phone = request.form.get("phone")
+        _size = request.form.get("size")
+        order = {"name": _name, 
+            "phone": _phone,
+            "size": _size}
+        orders.append(order)
+        print(orders)
+        return redirect(url_for('order_summery.html') orders=orders)
+    return render_template('pizza_order')
 
 if __name__ == "__main__":
     app.run(debug=True)
